1cd55dac5d9e53bacd0eda5a4bfecc74326eef6c
Michael J. Miller <commnerd@gmail.com>
2024-10-17 23:04:41 -0500

    Fund portfolio infrastructure set up

--- app/Models/Fund.php
+++ app/Models/Fund.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Fund extends Model
 {
@@ -29,5 +30,15 @@ class Fund extends Model
     public function aliases(): HasMany
     {
         return $this->hasMany(FundAlias::class);
+    }
+
+    /**
+     * Portfolio of invested companies
+     * 
+     * @return HasManyThrough
+     */
+    public function portfolio(): HasManyThrough
+    {
+        return $this->hasManyThrough(Company::class, CompanyFund::class);
     }
 }
